bbe6ed1b8a242dee4a3238f6180c6e516901caf4
francesc <francesc.guillen@gmail.com>
2015-04-04 15:58:43 +0200

    Re-organize code (test/no test). Delete old test.

--- src/kata_bcn/core.clj
+++ src/kata_bcn/core.clj
@@ -1,21 +1,6 @@
 (ns kata-bcn.core)
 (use 'clojure.test)
 
-
-(def all-zeros-game (take 20 (repeat 0)))
-
-(def all-ones-game (take 20 (repeat 1)))
-
-(def all-twos-game (take 20 (repeat 2)))
-
-(def a-game-with-a-strike-game (concat [10 "X"] (take 18 (repeat 2))))
-
-(def a-game-with-different-rolls (flatten (repeat 2 (range 10))))
-
-(def an-almost-perfect-game (flatten (concat (take 9 (repeat [10 "X"])) [8 2])))
-
-(def a-perfect-game (flatten (concat (take 9 (repeat [10 "X"])) [10 10 10])))
-
 (defn sum [x]
   (apply + x))
 
@@ -45,21 +30,33 @@
     (sum (map score-for-frame-with-accompanying-rolls (game->frames-with-accompanying-rolls game)))))
 
 
-
 ;;;;;;;;;;;;;
 ;;; Tests
 ;;;;;;;;;;;;;
 
-(are [x y] (= x y)
-     (score-for-frame-with-accompanying-frame [[1 2] [3 4]]) 3
-     (score-for-frame-with-accompanying-frame [[10 0] [3 4]]) 17
-     (score-for-frame-with-accompanying-frame [[6 4] [3 4]]) 13)
+(def all-zeros-game (take 20 (repeat 0)))
 
-(is (= '((10 0) (2 2) (2 2) (2 2) (2 2) (2 2) (2 2) (2 2) (2 2) (2 2))
-       (game->frames a-game-with-a-strike-game)))
+(def all-ones-game (take 20 (repeat 1)))
+
+(def all-twos-game (take 20 (repeat 2)))
+
+(def a-game-with-a-strike-game (concat [10 "X"] (take 18 (repeat 2))))
+
+(def a-game-with-different-rolls (flatten (repeat 2 (range 10))))
+
+(def an-almost-perfect-game (flatten (concat (take 9 (repeat [10 "X"])) [8 2])))
+
+(def a-perfect-game (flatten (concat (take 9 (repeat [10 "X"])) [10 10 10])))
+
+(are [x y] (= x y)
+     (score-for-frame-with-accompanying-rolls [[1 2] [3 4]]) 3
+     (score-for-frame-with-accompanying-rolls [[10 "X"] [3 4]]) 17
+     (score-for-frame-with-accompanying-rolls [[6 4] [3 4]]) 13)
 
 (are [x y] (= x y)
      0 (score all-zeros-game)
      20 (score all-ones-game)
      40 (score all-twos-game)
-     50 (score a-game-with-a-strike-game))
+     50 (score a-game-with-a-strike-game)
+     300 (score a-perfect-game)
+     268 (score an-almost-perfect-game))
